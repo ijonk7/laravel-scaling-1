1a06bafb959e1b78c724c8e1f79eff003943cbbe
ijonk7 <jookeo.rizal@gmail.com>
2021-01-10 04:15:50 +0700

    Delete kernel schedule (onOneServer)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('sl:example')->everyMinute()->onOneServer();
+        $schedule->command('sl:example')->everyMinute();
     }
 
     /**
