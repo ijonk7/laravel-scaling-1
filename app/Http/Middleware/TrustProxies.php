9636e40d741ec2bccd26fd9cdeeab879b4ed0552
ijonk7 <jookeo.rizal@gmail.com>
2021-01-09 22:51:14 +0700

    configured trusted proxy

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies;
+    protected $proxies = ['10.104.0.4'];
 
     /**
      * The headers that should be used to detect proxies.
